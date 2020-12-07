e27e90642c1aee43c6cbc5d87d37369e0bf13f29
liuvigongzuoshi <liuvigongzuoshi@foxmail.com>
2020-12-07 19:54:15 +0800

    chore: add cmd layout

--- cmd/ordinary-kriging-cli/main.go
+++ cmd/ordinary-kriging-cli/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
 func main() {
-	// TODO:
+	execute()
+
+}
+
+var cmd = &cobra.Command{
+	Use:   "go-kriging",
+	Short: "geospatial prediction and mapping via ordinary kriging",
+	Long: `Golang library for geospatial prediction and mapping via ordinary kriging.
+			Complete documentation is available at https://github.com/liuvigongzuoshi/go-kriging`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Go Kriging Version: v0.1.0")
+		// TODO:
+	},
+}
+
+func execute() {
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
--- cmd/ordinary-kriging-service/main.go
+++ cmd/ordinary-kriging-service/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
 func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/train", trainHandler)
+	http.HandleFunc("/grid", gridHandler)
+	server := &http.Server{
+		Addr:           ":8888",
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(server.ListenAndServe())
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", "ordinary-kriging")
+}
+
+func trainHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO:
+}
+
+func gridHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO:
 }
